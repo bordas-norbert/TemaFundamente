2a6158525f2885eca39dd581b8be9102331484f6
Bordás Norbert <b.norbert101@gmail.com>
2021-11-13 16:58:02 +0200

    add

---
+++ set1_1/PROJECT/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PROJECT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a=");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("b=");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("x={0}",(double)-b/a);
+        }
+    }
+}
